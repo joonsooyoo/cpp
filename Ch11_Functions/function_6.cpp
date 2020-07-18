4305682b175f58480b57935241d8610e4478a4dd
joonsooyoo <65941390+joonsooyoo@users.noreply.github.com>
2020-07-18 17:55:59 +0900

    an exercise is updated :)

---
+++ Ch11_Functions/function_6.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+/* 
+
+FUNCTION EXERCISE: using recursion
+
+*/
+
+// prototype
+double a_penny_doubled_everyday(int, double);
+
+int main() {
+
+	// set precision to 2
+	cout << fixed << setprecision(2);
+
+	// base_money, days
+	cout << "how much do you want to start? ";
+	double base_money {};
+	cin >> base_money;
+
+	cout << "how many days? "
+	int days {};
+	cin >> days;
+
+	// doubling recursion
+
+	return 0;
+}
+
+// definition
+double a_penny_doubled_everyday(int days, double money){
+
+	return 2 * money
+}
